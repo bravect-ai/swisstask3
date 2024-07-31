851a27acd53d43655c158af4961319c6cf547093
bravect-ai <116832035+bravect-ai@users.noreply.github.com>
2024-07-31 06:30:51 +0000

    swisstask3completed

--- scripts/mint.js
+++ scripts/mint.js
@@ -14,7 +14,7 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 };
 
 async function main() {
-  const contractAddress = "0x1E2015c85710140E000B87a05A98De818e288Ce0";
+  const contractAddress = "0x0E0091169ae03FeA830a8eef364Cfb76A2427aC4";
   const [signer] = await hre.ethers.getSigners();
   const contractFactory = await hre.ethers.getContractFactory("TestNFT");
   const contract = contractFactory.attach(contractAddress);
